4f557064da92065802da6b17698db6b5d93da2a5
emilienMuhayimana <149523246+emilienMuhayimana@users.noreply.github.com>
2023-11-04 16:09:48 +0200

    Update emileroan.java

--- emileroan.java
+++ emileroan.java
@@ -1,4 +1,5 @@
-public class emileroan {
+// this program show how to calculate the compound interest for three years
+public class emileroan { // this is the class name of this program
     
         private double principal;
         private double annualRate;
@@ -12,7 +13,7 @@ public class emileroan {
             this.monthsPerYear = monthsPerYear;
         }
     
-        public double calculateTotalPayment() {
+        public double calculateTotalPayment() { 
             double monthlyRate = (annualRate / 100) / monthsPerYear;
             int totalPayments = years * monthsPerYear;
             double totalPayment = principal * (monthlyRate * Math.pow(1 + monthlyRate, totalPayments)) / (Math.pow(1 + monthlyRate, totalPayments) - 1);
@@ -20,14 +21,14 @@ public class emileroan {
         }
     
         public static void main(String[] args) {
-            double principal = 500000.0; 
-            double annualRate = 18.0;   
-            int years = 3;              
+            double principal = 500000.0; // this is the initial deposit amount
+            double annualRate = 18.0;  // annual interest rate (5% )
+            int years = 3;     // this is the number of years         
             int monthsPerYear = 12;     
     
             emileroan roan = new emileroan(principal, annualRate, years, monthsPerYear);
             double totalPayment = roan.calculateTotalPayment();
-    
+    // this is the output of this program
             System.out.println("Total Payment after " + years + " years with compound interest: " + totalPayment + " RWF");
         }
     
