8f23e60e51cc569e180791b58d88219f0fc21651
emilienMuhayimana <149523246+emilienMuhayimana@users.noreply.github.com>
2023-11-04 15:37:55 +0200

    Update berak.java

--- berak.java
+++ berak.java
@@ -1,9 +1,9 @@
-public class berak {
+public class berak { //this is the class name of this program
 
 public static void main(String[] args) {
-for(int w=2;w<=9;w++){
+for(int w=2;w<=9;w++){ // this is the formura
 if(w==7) break;{
-System.out.println("w");
+System.out.println("w"); //this is the output of this program
 
 }
 }
