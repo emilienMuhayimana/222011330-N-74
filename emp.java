f839eadb48ab135aa5d91ce157097ba95e73c6ab
emilienMuhayimana <149523246+emilienMuhayimana@users.noreply.github.com>
2023-11-04 16:21:03 +0200

    Update emp.java

--- emp.java
+++ emp.java
@@ -1,8 +1,9 @@
-public class emp {
+// this program show the while condition
+public class emp {// class name
     public static void main(String[] args) {
-int i =2;
+int i =2; //decraletion variable
 while (i<=4) {
-System.out.println("rwanda");
+System.out.println("rwanda"); // the output
 i++;
 }
     
