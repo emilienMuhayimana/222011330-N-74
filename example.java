b86119ad157d8fad9e376a231d7195315c28d5e8
emilienMuhayimana <149523246+emilienMuhayimana@users.noreply.github.com>
2023-11-04 16:25:30 +0200

    Update example.java

--- example.java
+++ example.java
@@ -1,6 +1,8 @@
-public class example {
+//this program display for loop 
+public class example { // class name
 public static void main(String[]args){
   for(int i=1; i<=9;i++){
+  //this is the output
     System.out.println("arsenal");
   }
 }
