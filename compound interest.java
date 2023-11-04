2b2c7a32a12d4196aff7204048c1840e69f32292
emilienMuhayimana <149523246+emilienMuhayimana@users.noreply.github.com>
2023-11-04 17:20:50 +0200

    Update compound interest.java

--- compound interest.java
+++ compound interest.java
@@ -1,12 +1,12 @@
 // this program show how to calculate the compound interest for three years
-public class emileroan { // this is the class name of this program
+public class compound interest { // this is the class name of this program
     
         private double principal;
         private double annualRate;
         private int years;
         private int monthsPerYear;
     
-        public emileroan(double principal, double annualRate, int years, int monthsPerYear) {
+        public compound interest(double principal, double annualRate, int years, int monthsPerYear) {
             this.principal = principal;
             this.annualRate = annualRate;
             this.years = years;
@@ -26,7 +26,7 @@ public class emileroan { // this is the class name of this program
             int years = 3;     // this is the number of years         
             int monthsPerYear = 12;     
     
-            emileroan roan = new emileroan(principal, annualRate, years, monthsPerYear);
+            compound interest roan = new compound interest(principal, annualRate, years, monthsPerYear);
             double totalPayment = roan.calculateTotalPayment();
     // this is the output of this program
             System.out.println("Total Payment after " + years + " years with compound interest: " + totalPayment + " RWF");
