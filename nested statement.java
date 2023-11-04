b30599616ab419deab6c8f790826d96b411a6ce4
emilienMuhayimana <149523246+emilienMuhayimana@users.noreply.github.com>
2023-11-04 16:40:04 +0200

    Update and rename nested.java to nested statement.java

---
+++ nested statement.java
@@ -0,0 +1,12 @@
+// this program display nested statement by the increament
+public class nested { //class name
+
+public static void main(String[] args) {
+for(int a=1;a<=4;a++){
+for(int b=1;b<=5;b++){
+// this is the output
+  System.out.println("a+ +b");
+}
+}
+}
+}
