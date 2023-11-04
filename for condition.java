949791665677d740bbf19ea52a2a06bc9543aa0a
emilienMuhayimana <149523246+emilienMuhayimana@users.noreply.github.com>
2023-11-04 17:31:13 +0200

    Update for condition.java

--- for condition.java
+++ for condition.java
@@ -1,5 +1,5 @@
 //this program display for loop 
-public class example { // class name
+public class for condition { // class name
 public static void main(String[]args){
   for(int i=1; i<=9;i++){
   //this is the output
