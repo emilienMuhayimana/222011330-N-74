ce60b83b5400055e306b5d31a6fd7040781eae47
emilienMuhayimana <149523246+emilienMuhayimana@users.noreply.github.com>
2023-11-04 17:11:44 +0200

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,11 +1,14 @@
-public class sum {
+// this program display the sum of two numbers
+public class sum { // class name
 public static int add(int num1,int num2) {
 return num1+num2;}
 public static void main(String[]args){
-int number1=4;
+// this is initialisation of variable
+    int number1=4;
 int number2=7;
 int result = add(number1,number2);
-System. out.println("the sum of" +number1 +"and"+number2 +"is:"+result);
+// this is the output
+    System. out.println("the sum of" +number1 +"and"+number2 +"is:"+result);
 }
     
 } 
