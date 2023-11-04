9262cc6dd2580c4e2deec68a3a66ac07cb18b6bb
emilienMuhayimana <149523246+emilienMuhayimana@users.noreply.github.com>
2023-11-04 16:32:55 +0200

    Update and rename key.java to do while.java

---
+++ do while.java
@@ -0,0 +1,12 @@
+//this program display the increament number by using do while condition
+public class key { // class name
+public static void main(String[] args) {
+int h=1; // variable
+do{
+//this is the output
+  System.out.println("rubavu");
+h++;}
+while(h<=3);
+}    
+}  
+
