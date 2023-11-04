0aa87a4f58f31c9a3f4b0141d304df7bede22aac
emilienMuhayimana <149523246+emilienMuhayimana@users.noreply.github.com>
2023-11-04 17:14:11 +0200

    Update th.java

--- th.java
+++ th.java
@@ -1,7 +1,8 @@
-public class th {
+// this program show the for loop statement
+public class th { // class name
     public static void main(String[] args) {
 for(int i=1;i<=4;i++){
-System.out.println("kenya");
+System.out.println("kenya"); // output
 }
     }
 }
