0e0c3cdb43c0f80cd1eef291fa15e24a0e49434a
emilienMuhayimana <149523246+emilienMuhayimana@users.noreply.github.com>
2023-11-09 10:44:22 +0200

    Update inheritence.java

--- inheritence.java
+++ inheritence.java
@@ -1,22 +1,22 @@
-
+// this program will display diffrence messages by using java inheritance
 class A  {
-void get(){
+void get(){ //this the method
 System.out.println("hey");}
         }
-class B extends C{//class A extends class B
-void show(){
+class B extends C{//class B extends class A
+void show(){ //method
 
-System.out.println("go there");}//output
+System.out.println("go there");}// this is output
 }
-class C extends D{//class B extends C
-void display(){
-System.out.println("welcome to mr muhorwa");}
+class C extends D{//class C extends B
+void display(){ //method
+System.out.println("welcome to mr muhorwa");} // this is the output
 }
-class D{//class C extends D
-void like(){
-System.out.println("its location");}
+class D{//class D extends C
+void like(){ //method
+System.out.println("its location");} //this is output
 }
-interface E{
+interface E{ // this the interface
 void printE();
 }
  
@@ -30,15 +30,15 @@ void printE();
 /**
  * inheritence
  */
-public class inheritence {
+public class inheritence { //this is the class name or main class
 
 public static void main(String[] args) {
-A hec = new A();
-B bar = new B();
-C mn = new C();
-D rh = new D();
+A hec = new A(); // hec is object name
+B bar = new B(); //bar is object name
+C mn = new C(); //mn is object name
+D rh = new D(); //rh is object name
 
-
+// calling method
 hec.get();
 bar.show();
 mn.display();
